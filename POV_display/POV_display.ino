586289885e3b9974a6259930f9dd101cea8e1767
Alex <beragumbo@ya.ru>
2018-01-15 23:49:46 +0300

    add

---
+++ POV_display/POV_display.ino
@@ -0,0 +1,339 @@
+// ------------------ НАСТРОЙКИ ------------------
+#define TRIGGER 12        // пин кнопки запуска анимации
+#define SPACE 2           // интервал между буквами
+#define CUSTOM_AMOUNT 8   // число спецсимволов
+#define BLUETOOTH_MODE 0  // если схема с bluetooth модулем
+// ------------------ НАСТРОЙКИ ------------------
+
+// ------------------ ДЛЯ РАЗРАБОТЧИКОВ ------------------
+#include "fonts.h"
+#include "EEPROMex.h"
+
+#if (BLUETOOTH_MODE == 1)    // если режим блютуса
+#include <SoftwareSerial.h>
+SoftwareSerial btSerial(10, 11); // RX, TX
+#define Serial btSerial      // заменить Serial на btSerial во всём коде ниже
+#endif
+
+byte availableBytes, frames;
+boolean recievedFlag;
+String frameString = "";
+String strData = "";
+String speedSet = ".speed";
+String customSet = ".custom";
+String reverseSet = ".reverse";
+String loopSet = ".loop";
+byte Speed;
+boolean loopMode, reverseMode, customMode;
+boolean anim_flag, trigFlag, offlineMode;
+// ------------------ ДЛЯ РАЗРАБОТЧИКОВ ------------------
+
+void setup() {
+  // ------ ПЕРВЫЙ ЗАПУСК -----
+  if (EEPROM.read(100) != 1) {   // в ячейке 100 должен быть замисан флажок 1, если его нет - делаем
+    EEPROM.writeByte(100, 1);
+    EEPROM.updateByte(101, 40);
+    EEPROM.updateByte(102, 0);
+    EEPROM.updateByte(103, 0);
+    EEPROM.updateByte(104, 5);
+    EEPROM.updateByte(105, 0);
+  }
+
+  pinMode(TRIGGER, INPUT_PULLUP);            // пин кнопки подтянуть
+
+  if (!digitalRead(TRIGGER)) {               // если при запуске нажата кнопка
+    offlineMode = true;                      // запускаемся в оффлайн режиме
+    DDRD = B11111111;                        // пины 0-7 как выходы
+    PORTD = 0xff;                            // включить все
+    while (!digitalRead(TRIGGER));           // ждём отпускания кнопки
+    delay(100);
+    PORTD = 0x00;                            // выключить все
+  }
+  while (!digitalRead(TRIGGER));             // ждём отпускания кнопки
+
+  if (BLUETOOTH_MODE) {
+    offlineMode = true;
+    DDRD = B11111111;                        // пины 0-7 как выходы
+    PORTD = 0x00;
+  }
+  /*
+     EEPROM MAP
+     0-64 - буфер
+     100 - первый запуск
+     101 - скорость
+     102 - реверс
+     103 - луп
+     104 - длина буфера
+     105 - загружен ли кастом
+  */
+  // загружаем настройки из памяти
+  Speed = EEPROM.read(101);
+  reverseMode = EEPROM.read(102);
+  loopMode = EEPROM.read(103);
+  frames = EEPROM.read(104);
+  customMode = EEPROM.read(105);
+
+  // загружаем буфер анимации из памяти
+  for (byte i = 0; i < frames; i++) {
+    frameString += (char)EEPROM.read(i);
+  }
+
+  if (!offlineMode || BLUETOOTH_MODE) {  // если не оффлайн режим ИЛИ блютус режим
+    Serial.begin(9600);
+    Serial.println(F("Welcome to GyverPOV!"));
+    Serial.println(F("You can use some serial commands:"));
+    Serial.println(F("Send any text то put in in the animation buffer"));
+    Serial.println(F("Send .customNUM with NUM 0-7 to load custom symbol (example .custom3)"));
+    Serial.println(F("Send .speedVALUE with VALUE 0-100 to set speed (example .speed40)"));
+    Serial.println(F("Send .reverse0 to animation left-to-right or .reverse1 to right-to-left"));
+    Serial.println(F("Send .loop0 to disable animation repeating or .loop1 to enable it"));
+    Serial.println(F("Any changes will be instantly saved in EEPROM"));
+    Serial.println(F("Hold trigger button before power up to load system in offline mode"));
+
+    Serial.print(F("Loaded preset is "));
+    if (customMode) Serial.print(F("custom symbol"));
+    else {
+      Serial.print(F("text <"));
+      Serial.print(frameString);
+      Serial.print(F(">"));
+    }
+    Serial.print(F(" with ")); Serial.print(frames);
+    if (reverseMode) Serial.print(F(" reversed "));
+    else Serial.print(F(" not reversed "));
+    Serial.print(F("frames, "));
+    Serial.print(Speed);
+    Serial.print(F(" ms interval, "));
+    if (loopMode) Serial.print(F("looped. "));
+    else Serial.print(F("not looped. "));
+    Serial.println(F("Enjoy!"));
+    Serial.println();
+  }
+}
+
+void loop() {
+  //------------------------------------------- SERIAL LOOP ------------------------------------------
+  for (;;) {         // главный подцикл, ожидает данные в Serial и отрабатывает кнопку
+    triggerTick();
+    availableBytes = Serial.available();         // считаем, сколько байт в буфере
+    if (availableBytes > 0 && !anim_flag) {      // если есть что то на вход и НЕ показывается анимация
+      delay(200);                                // ждём, пока придут остальные символы
+      availableBytes = Serial.available();       // обновляем число байт в буфере
+      strData = "";                              // очистить строку
+      for (int i = 0; i < availableBytes; i++) {
+        char newByte = Serial.read();
+        strData += newByte;                      // забиваем строку принятыми данными
+      }
+      recievedFlag = true;                       // поднять флаг что получили данные
+      break;                                     // выходим из цикла
+    }
+  }
+  //------------------------------------------- SERIAL LOOP ------------------------------------------
+
+
+  //----------------------------------------- ОБРАБОТКА ДАННЫХ ----------------------------------------
+  if (recievedFlag) {
+    // ----- НАСТРОЙКА СКОРОСТИ АНИМАЦИИ -----
+    if (strData.startsWith(speedSet)) {         // если строка начинается с
+      strData.remove(0, 6);                     // обрезать строку, оставить только значение
+      byte newSpeed = strData.toInt();          // перевести в int
+      if (newSpeed > 0 && newSpeed < 100) {     // если лежит в диапазоне
+        Serial.print(F("Speed set: "));
+        Speed = newSpeed;
+        Serial.println(Speed);
+        updateEEPROM();
+      }
+      else Serial.println(F("Wrong speed!"));   // если не лежит
+
+      // ----- НАСТРОЙКА ЦИКЛИЧНОСТИ -----
+    } else if (strData.startsWith(loopSet)) {
+      strData.remove(0, 5);
+      byte newLoop = strData.toInt();
+      if (newLoop == 1) {
+        loopMode = true;
+        Serial.println(F("Loop mode on"));
+        updateEEPROM();
+      } else if (newLoop == 0) {
+        loopMode = false;
+        Serial.println(F("Loop mode off"));
+      }
+      else Serial.println(F("Wrong loop!"));
+
+      // ----- НАСТРОЙКА РЕВЕРСА -----
+    } else if (strData.startsWith(reverseSet)) {
+      strData.remove(0, 8);
+      byte newReverse = strData.toInt();
+      if (newReverse == 1) {
+        reverseMode = true;
+        updateEEPROM();
+        Serial.println(F("Reverse on"));
+      } else if (newReverse == 0) {
+        reverseMode = false;
+        Serial.println(F("Reverse off"));
+      }
+      else Serial.println(F("Wrong reverse!"));
+
+      // ----- СВОЙ СИМВОЛ -----
+    } else if (strData.startsWith(customSet)) {
+      strData.remove(0, 7);
+      byte newCustom = strData.toInt();
+      if (newCustom >= 0 && newCustom < CUSTOM_AMOUNT) {
+        loadCustom(newCustom);
+        Serial.print(F("Custom #"));
+        Serial.print(newCustom);
+        Serial.println(F(" is loaded"));
+      }
+      else Serial.println(F("Wrong custom number!"));
+      customMode = true;
+      updateEEPROM();
+
+    } else {
+      frameString = strData;
+      frames = availableBytes;
+      Serial.print(F("Animation text: "));
+      Serial.println(frameString);
+      customMode = false;
+      updateEEPROM();
+    }
+    recievedFlag = false;
+    availableBytes = 0;
+  }
+  //----------------------------------------- ОБРАБОТКА ДАННЫХ ----------------------------------------
+
+}
+
+// функция загрузки своего символа в строку (можно было сделать красивее, но мне было лень)
+void loadCustom(byte newCustom) {
+  switch (newCustom) {
+    case 0: fillCustom(custom0, sizeof(custom0)); break;
+    case 1: fillCustom(custom1, sizeof(custom1)); break;
+    case 2: fillCustom(custom2, sizeof(custom2)); break;
+    case 3: fillCustom(custom3, sizeof(custom3)); break;
+    case 4: fillCustom(custom4, sizeof(custom4)); break;
+    case 5: fillCustom(custom5, sizeof(custom5)); break;
+    case 6: fillCustom(custom6, sizeof(custom6)); break;
+    case 7: fillCustom(custom7, sizeof(custom7)); break;
+  }
+}
+
+// забивка строки байтами символа
+void fillCustom(uint8_t thisCustom[], byte frames_amount) {
+  frames = frames_amount;
+  frameString = "";
+  for (int i = 0; i < frames; i++) {
+    char newByte = thisCustom[i];
+    frameString += newByte;                      // забиваем строку принятыми данными
+  }
+}
+
+// ----------------------- ОТРАБОТКА КНОПКИ ---------------------
+void triggerTick() {
+  if (!digitalRead(TRIGGER) && !trigFlag) {
+    trigFlag = true;
+    anim_flag = true;  // бесполезный флаг, запрещающий общение с портом. Но бережёного бох бережёт
+    if (!offlineMode)Serial.println(F("Animation start"));
+    delay(20);
+    if (!offlineMode)Serial.end();
+    if (!offlineMode)DDRD = B11111111;  // пины 0-7 как выходы
+    showAnimation();
+    delay(10);
+  }
+  if (digitalRead(TRIGGER) && trigFlag) {
+    if (!offlineMode)DDRD = B00000000;  // пины 0-7 как входы
+    if (!offlineMode)Serial.begin(9600);
+    delay(10);
+    trigFlag = false;
+    anim_flag = false;
+    if (!offlineMode)Serial.println();
+    if (!offlineMode)Serial.println(F("Animation end"));
+    delay(100);
+  }
+}
+// ----------------------- ОТРАБОТКА КНОПКИ ---------------------
+
+void updateEEPROM() {
+  // ----- СОХРАНЕНИЕ -----
+  /*
+    EEPROM MAP
+    0-64 - буфер
+    100 - первый запуск
+    101 - скорость
+    102 - реверс
+    103 - луп
+    104 - длина буфера
+    105 - загружен ли кастом
+  */
+
+  for (byte i = 0; i < frames; i++) {
+    EEPROM.updateByte(i, frameString[i]);
+  }
+  EEPROM.updateByte(101, Speed);
+  EEPROM.updateByte(102, reverseMode);
+  EEPROM.updateByte(103, loopMode);
+  EEPROM.updateByte(104, frames);
+  if (customMode) EEPROM.updateByte(105, 1);
+  else EEPROM.updateByte(105, 0);
+}
+
+// -------------------------------- АНИМАЦИЯ ------------------------------
+void showAnimation() {
+  for (;;) {                        // бесконечный цикл отрисовки
+    if (reverseMode) {              // если реверс
+      if (!customMode) {            // если ТЕКСТ
+        for (int8_t j = (frames - 1); j >= 0; j--) {
+          char newRow = frameString[j];          // взять букву из строки
+          for (int8_t i = 4; i >= 0; i--) {      // для пяти столбцов буквы
+            PORTD = getFont(newRow, i);          // получить её битмап и отправить на светодиоды
+            delay(Speed);                        // задержка кадра
+          }
+          for (byte i = 0; i < SPACE; i++) {     // буква написана, теперь вставляем пробелы
+            PORTD = 0x00;                        // рисуем пустой столбец
+            delay(Speed);                        // ждём
+          }
+        }
+      } else {                      // если СИМВОЛ
+        for (int8_t j = (frames - 1); j >= 0; j--) {
+          PORTD = frameString[j];
+          delay(Speed);
+        }
+        PORTD = 0x00;
+      }
+    } else {
+      if (!customMode) {
+        for (byte j = 0; j < frames; j++) {
+          char newRow = frameString[j];
+          for (byte i = 0; i < 5; i++) {
+            PORTD = getFont(newRow, i);
+            delay(Speed);
+          }
+          for (byte i = 0; i < SPACE; i++) {
+            PORTD = 0x00;
+            delay(Speed);
+          }
+        }
+      } else {
+        for (byte j = 0; j < frames; j++) {
+          PORTD = frameString[j];
+          delay(Speed);
+        }
+        PORTD = 0x00;
+      }
+    }
+
+
+    if (!loopMode) {                      // если нарисовали всё что хотели, и режим зацикливания НЕ ВКЛЮЧЕН
+      break;                              // покинуть цикл отрисовки
+    }
+    else if (digitalRead(TRIGGER)) {      // если цикл ВКЛЮЧЕН, но ОТПУЩЕНА кнопка, покинуть цикл отрисовки
+      if (!offlineMode)DDRD = B00000000;  // пины как входы
+      if (!offlineMode)Serial.begin(9600);  // запустить связь
+      delay(10);
+      trigFlag = false;
+      anim_flag = false;
+      if (!offlineMode)Serial.println();
+      if (!offlineMode)Serial.println(F("Animation end"));
+      delay(400);
+      break;
+    }
+  }
+}
+// -------------------------------- АНИМАЦИЯ ------------------------------
--- README.md
+++ README.md
@@ -1,1 +1,131 @@
-# POV_Serial
+[![AlexGyver YouTube](http://alexgyver.ru/git_banner.jpg)](https://www.youtube.com/channel/UCgtAOyEQdAyjvm9ATCi_Aig?sub_confirmation=1)
+# POV писалка - рисовалка с настройкой по UART
+* [Описание проекта](#chapter-0)
+* [Папки проекта](#chapter-1)
+* [Схемы подключения](#chapter-2)
+* [Материалы и компоненты](#chapter-3)
+* [Как скачать и прошить](#chapter-4)
+* [FAQ](#chapter-5)
+* [Полезная информация](#chapter-6)
+
+<a id="chapter-0"></a>
+## Описание проекта
+Простейшая POV рисовалка на 8 светодиодах
+- Ввод текста для отрисовки через монитор порта или Bluetooth
+- Отрисовка всех символов, латинских и русских букв (все битмапы в памяти)
+- Изменение скорости анимации
+- Изменение направления анимации
+- Включение-выключение зацикленности анимации
+- Сохранение настроек в EEPROM
+- Отрисовка графики 8 пикселей в высоту и неограниченно в длину
+- Подробности в видео: https://youtu.be/hCPOQI-eFYs
+
+<a id="chapter-1"></a>
+## Папки
+**ВНИМАНИЕ! Если это твой первый опыт работы с Arduino, читай [инструкцию](#chapter-4)**
+- **libraries** - библиотеки проекта. Заменить имеющиеся версии
+- **POV_display** - прошивка для Arduino, файл в папке открыть в Arduino IDE ([инструкция](#chapter-4))
+- **schemes** - схемы
+
+<a id="chapter-2"></a>
+## Схемы
+![SCHEME](https://github.com/AlexGyver/PCdisplay/blob/master/schemes%26PCBs/scheme_fritzing.png)
+![SCHEME](https://github.com/AlexGyver/PCdisplay/blob/master/schemes%26PCBs/scheme_EasyEDA.png)
+
+<a id="chapter-3"></a>
+## Материалы и компоненты
+* Arduino NANO http://ali.pub/20o35g  http://ali.pub/20o36t
+* Дисплей 2004 синий с драйвером http://ali.pub/20o3dt  http://ali.pub/20o3el
+* Дисплей 1602 синий с драйвером http://ali.pub/21wqmn http://ali.pub/21wqla
+* Драйвер дисплея отдельно http://ali.pub/21wqos
+* Дисплеи 1602 БЕЗ ДРАЙВЕРА (купить отдельно):
+  - Синий фон, белые буквы http://ali.pub/21wr11
+  - Зелёный фон, чёрные буквы http://ali.pub/21wqv7  
+  - Чёрный фон, рыжие буквы http://ali.pub/21wr27
+  - Чёрный фон, белые буквы http://ali.pub/21wr3y
+  - Серый фон, чёрные буквы http://ali.pub/21wrhr
+* Провод http://ali.pub/20oa1k  http://ali.pub/20oa3w
+* Датчик температуры http://ali.pub/20o3fk  http://ali.pub/20o3gc
+* Лента светодиодная http://ali.pub/20o51w  http://ali.pub/20o53u
+* Макетная плата http://ali.pub/20o3nj  http://ali.pub/20o3nx
+* Клеммники винтовые http://ali.pub/20o3ty
+* 10 МОСФЕТов http://ali.pub/20o1yh
+* 100 Ом http://ali.pub/20o33b
+* 10 кОм http://ali.pub/20o334
+* Кнопки ищите на радиорынке, у китайцев можно 500 штук только купить
+* Или тут смотрите http://alexgyver.ru/electronics/
+* Или такие http://ali.pub/20o4vo
+* Корпус http://ali.pub/20ocky  http://ali.pub/20ocpm
+
+РАССЫПУХА В РОССИИ
+* Светодиод https://www.chipdip.ru/product/irf3205
+* Резистор https://www.chipdip.ru/product/irfz24n
+* Кнопка https://www.chipdip.ru/product0/47324
+
+## Вам скорее всего пригодится
+* [Всё для пайки (паяльники и примочки)](http://alexgyver.ru/all-for-soldering/)
+* [Недорогие инструменты](http://alexgyver.ru/my_instruments/)
+* [Все существующие модули и сенсоры Arduino](http://alexgyver.ru/arduino_shop/)
+* [Электронные компоненты](http://alexgyver.ru/electronics/)
+* [Аккумуляторы и зарядные модули](http://alexgyver.ru/18650/)
+
+<a id="chapter-4"></a>
+## Как скачать и прошить
+* [Первые шаги с Arduino](http://alexgyver.ru/arduino-first/) - ультра подробная статья по началу работы с Ардуино, ознакомиться первым делом!
+* Скачать архив с проектом
+> На главной странице проекта (где ты читаешь этот текст) вверху справа зелёная кнопка **Clone or download**, вот её жми, там будет **Download ZIP**
+* Установить библиотеки в  
+`C:\Program Files (x86)\Arduino\libraries\` (Windows x64)  
+`C:\Program Files\Arduino\libraries\` (Windows x86)
+* Подключить Ардуино к компьютеру
+* Запустить файл прошивки (который имеет расширение .ino)
+* Настроить IDE (COM порт, модель Arduino, как в статье выше)
+* Настроить что нужно по проекту
+* Нажать загрузить
+* Пользоваться  
+  
+**Управление POV рисовалкой через монитор порта или Bluetooth**
+* Любой обычный текст (не команда) будет записан в память для отрисовки
+* Команда .customNUM, где NUM = 0-7: загрузка символа под номером NUM (пример .custom3)
+* Команда .speedVALUE, где VALUE = 0-100 установить скорость анимации (пример .speed40)
+* Команда .reverse0 для анимации слева направо и .reverse1 справа налево
+* Команла .loop0 отключить зацикливание и .loop1 включить
+* Удерживай кнопку при подаче питания, чтобы войти в оффлайн-режим
+
+
+## Настройки в коде
+    #define TRIGGER 12        // пин кнопки запуска анимации
+    #define SPACE 2           // интервал между буквами
+    #define CUSTOM_AMOUNT 8   // число спецсимволов
+    #define BLUETOOTH_MODE 0  // если схема с bluetooth модулем (1 да, 0 нет)
+
+<a id="chapter-5"></a>
+## FAQ
+### Основные вопросы
+В: Как скачать с этого грёбаного сайта?  
+О: На главной странице проекта (где ты читаешь этот текст) вверху справа зелёная кнопка **Clone or download**, вот её жми, там будет **Download ZIP**
+
+В: Скачался какой то файл .zip, куда его теперь?  
+О: Это архив. Можно открыть стандартными средствами Windows, но думаю у всех на компьютере установлен WinRAR, архив нужно правой кнопкой и извлечь.
+
+В: Я совсем новичок! Что мне делать с Ардуиной, где взять все программы?  
+О: Читай и смотри видос http://alexgyver.ru/arduino-first/
+
+В: Компьютер никак не реагирует на подключение Ардуины!  
+О: Возможно у тебя зарядный USB кабель, а нужен именно data-кабель, по которому можно данные передавать
+
+В: Ошибка! Скетч не компилируется!  
+О: Путь к скетчу не должен содержать кириллицу. Положи его в корень диска.
+
+В: Сколько стоит?  
+О: Ничего не продаю.
+
+### Вопросы по этому проекту
+
+<a id="chapter-6"></a>
+## Полезная информация
+* [Мой сайт](http://alexgyver.ru/)
+* [Основной YouTube канал](https://www.youtube.com/channel/UCgtAOyEQdAyjvm9ATCi_Aig?sub_confirmation=1)
+* [YouTube канал про Arduino](https://www.youtube.com/channel/UC4axiS76D784-ofoTdo5zOA?sub_confirmation=1)
+* [Мои видеоуроки по пайке](https://www.youtube.com/playlist?list=PLOT_HeyBraBuMIwfSYu7kCKXxQGsUKcqR)
+* [Мои видеоуроки по Arduino](http://alexgyver.ru/arduino_lessons/)
